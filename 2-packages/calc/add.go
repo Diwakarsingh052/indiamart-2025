1a8f2afed6d680fe92065c5b6ea157a466e1e94b
Diwakar Singh <diwakarsingh052@gmail.com>
2025-10-07 11:15:35 +0530

    .

--- 2-packages/calc/add.go
+++ 2-packages/calc/add.go
@@ -2,6 +2,9 @@ package calc
 
 // If function first letter is capital, it is exported
 // otherwise it is not exported, which means it can only be used in the same package
+
+// Exporting means a type or function can be used outside the package
+
 func Add(a int, b int) {
 	sum := a + b
 
