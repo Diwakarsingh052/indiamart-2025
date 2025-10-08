0d5fee3cf062edbd76aa230c971d7e026efe6e18
Diwakar Singh <diwakarsingh052@gmail.com>
2025-10-08 11:24:03 +0530

    .

--- 7-concurrency/6-anonymous-goroutine.go
+++ 7-concurrency/6-anonymous-goroutine.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	// new returns a pointer to the type, type would be initialized to its default values
+	wg := new(sync.WaitGroup)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("work done")
+	}()
+
+	fmt.Println("end of the main")
+	wg.Wait()
+
+}
