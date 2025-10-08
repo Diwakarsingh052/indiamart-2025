07183dfdde51b0554ac9b2d425d1b9efac549771
Diwakar Singh <diwakarsingh052@gmail.com>
2025-10-08 14:40:32 +0530

    .

---
+++ 7-concurrency/15-concurrency.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	wg := new(sync.WaitGroup)
+
+	ch := make(chan int, 5)
+	go func() {
+		for i := 1; i <= 6; i++ {
+			go func() {
+				// fan out pattern
+				// spinning n number of goroutines for n tasks
+				ch <- i
+			}()
+
+		}
+		fmt.Println("all values sent")
+		// sends a signal to stop the range
+		//close(ch)
+		// close signal range that no more values be sent
+		//and it can stop after receiving remaining values
+
+	}()
+
+	go func() {
+
+		// it would run infinitely, channel needs to be closed to stop this range
+		// if channel is closed range will receive remaining values and stop
+		for i := range ch { // range over channel is a receive operation
+			time.Sleep(1 * time.Second)
+			fmt.Println(i, "range 1")
+		}
+	}()
+
+	wg.Wait()
+}
