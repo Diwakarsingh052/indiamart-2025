b1b563f3bef95627b4ca9c64f5a731ccfe8d891c
Diwakar Singh <diwakarsingh052@gmail.com>
2025-10-09 09:29:02 +0530

    .

--- 9-context/2-ctx.go
+++ 9-context/2-ctx.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"strconv"
 	"time"
 )
@@ -10,7 +9,6 @@ import (
 // Context must be passed as the first argument
 func Slow(ctx context.Context, msg string) (int, error) {
 	i, err := strconv.Atoi(msg)
-	sql.DB{}.ExecContext()
 	time.Sleep(2 * time.Second)
 	if err != nil {
 		return 0, err
