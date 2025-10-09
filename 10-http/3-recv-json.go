d172adbf9569e7cf66280d549eb3cb843181427c
Diwakar Singh <diwakarsingh052@gmail.com>
2025-10-09 12:00:55 +0530

    .

--- 10-http/3-recv-json.go
+++ 10-http/3-recv-json.go
@@ -44,3 +44,30 @@ func processJson(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"status": "ok"}`))
 
 }
+
+/*
+curl -X POST "http://localhost:8080/process-json" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "glossary": {
+      "title": "example glossary",
+      "GlossDiv": {
+        "title": "S",
+        "GlossList": {
+          "GlossEntry": {
+            "ID": "SGML",
+            "SortAs": "SGML",
+            "GlossTerm": "Standard Generalized Markup Language",
+            "Acronym": "SGML",
+            "Abbrev": "ISO 8879:1986",
+            "GlossDef": {
+              "para": "A meta-markup language, used to create markup languages such as DocBook.",
+              "GlossSeeAlso": ["GML", "XML"]
+            },
+            "GlossSee": "markup"
+          }
+        }
+      }
+    }
+  }'
+*/
