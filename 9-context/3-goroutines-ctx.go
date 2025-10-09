da09b388b501290eb07b4af32a705dcd12e4515a
Diwakar Singh <diwakarsingh052@gmail.com>
2025-10-09 10:39:44 +0530

    .

---
+++ 9-context/3-goroutines-ctx.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+	wg := new(sync.WaitGroup)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	wg.Go(func() {
+		i := slowFunc()
+		ch <- i
+	})
+
+	select {
+	case <-ctx.Done():
+		fmt.Println(ctx.Err())
+	case x := <-ch:
+		fmt.Println("received", x)
+
+	}
+
+}
+
+func slowFunc() int {
+	time.Sleep(2 * time.Second)
+	fmt.Println("slowFunc() ran and changed 10 files")
+	return 10
+
+}
