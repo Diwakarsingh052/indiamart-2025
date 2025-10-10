40781fe11a5f9e03d90d1fc5988a196e21e65f94
Diwakar Singh <diwakarsingh052@gmail.com>
2025-10-10 10:30:32 +0530

    .

---
+++ 12-grpc/proto-buf-basics/go.mod
@@ -0,0 +1,5 @@
+module proto-buf-basics
+
+go 1.25.1
+
+require google.golang.org/protobuf v1.36.10
---
+++ 12-grpc/proto-buf-basics/main.go
@@ -0,0 +1,8 @@
+package main
+
+//https://grpc.io/docs/languages/go/quickstart/
+//https://protobuf.dev/programming-guides/proto3/
+
+func main() {
+
+}
